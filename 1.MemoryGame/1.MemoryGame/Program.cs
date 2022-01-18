a2299b79f465404f67fc28678469f7f27d4e1df5
DavidMo26 <97259096+DavidMo26@users.noreply.github.com>
2022-01-18 18:25:46 +0200

    Add files via upload

---
+++ 1.MemoryGame/1.MemoryGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1.MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game("X","Y");
+            game.Play();          
+        }
+    }
+}
